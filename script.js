60cd3e647be5c5062ffa5819fe44f890a9946832
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-20 17:15:23 +0530

    Almost javaScript function are running and our calculater are working good

---
+++ script.js
@@ -0,0 +1,15 @@
+let string = "";
+buttons = document.querySelectorAll(".button");
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if (e.target.innerHTML == "=") {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        } else {
+            console.log(e.target);
+            string += e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+});
